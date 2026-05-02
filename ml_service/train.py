2de4eb80dac91e3cd557a19951db5a3a8a047669
HAONQ231205 <nguyenquanghao23122005@gmail.com>
2026-05-02 11:32:49 +0700

    resolve merge conflict

--- ml_service/train.py
+++ ml_service/train.py
@@ -6,6 +6,33 @@ from ml_service.traffic_predictor import TrafficPredictor
 SCALE_FACTOR = 450.0 / 815.0   # ≈ 0.55
 
 
+def light_green_time():
+    base_dir = os.path.dirname(__file__)
+    file_path = os.path.join(base_dir, '../ml_service/data/urban_traffic.csv')
+
+    df = pd.read_csv(file_path)
+
+
+    time = 45
+    avg = df['Target_Vehicle_Count'].mean()
+
+    print(avg)
+
+    df['time-green-light'] = 45
+
+    for i in range(len(df)):
+        t = df.iloc[i]['time-green-light']
+
+        delta = (abs(df.iloc[i]['Target_Vehicle_Count'] - avg) / avg) * 100
+        t = t + (delta // 10) * 5
+
+        if df.iloc[i]['Peak_Off_Peak'] == "Peak":
+            t += 10
+        df.at[i, 'time-green-light'] = t
+
+    print(df[['Target_Vehicle_Count', 'Peak_Off_Peak', 'time-green-light']].head())
+    df.to_csv(file_path, index=False)
+    
 def main():
     base = os.path.dirname(__file__)
     raw_csv = os.path.join(base, 'data/Metro_Interstate_Traffic_Volume.csv')
